5f53389d4c3b63106fc0e0459a7338d68d95b33d
jindongyang94 <jindongy@gmail.com>
2019-07-04 17:47:52 +0800

    empty cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='sample pipeline on how luigi can be used as ml pipeline',
+    author='jindongyang',
+    license='',
+)
